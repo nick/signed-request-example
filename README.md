dfd25f610c75150d08f93777da96bf99c933b901
Nick Poulden <nick@poulden.com>
2016-08-29 15:59:43 -0600

    Updates

---
+++ README.md
@@ -0,0 +1,8 @@
+# Example OAuth app
+
+This example takes a signed_request body and displays an auth token.
+
+## Installation
+
+1. Sign up with Heroku
+2. Install the [Heroku toolbelt](https://devcenter.heroku.com/articles/heroku-command-line)
--- index.js
+++ index.js
@@ -1,59 +1,50 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    crypto = require('crypto');
-
-var app = express();
+    crypto = require('crypto'),
+    app = express();
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const AF_URL = 'http://bd9:5000';
-const BASE_URL = 'http://localhost:3000'
-
-var egHTML = (vars) => `<!DOCTYPE html>
-<html>
-  <head><title>Oauth App</title></head>
-  <body>
-    <div style="text-align: center; padding-top: 10px">
-      <p>I'm an authroized app!</p>
-      <p>Token: ${vars.token}</p>
-      <script>parent.postMessage("loaded", '*')</script>
-    </div>
-  </body>
-</html>`;
 
 app.get('/', (req, res) => res.send('Hello, world!'));
 
 app.post('/canvas', function(req, res, next) {
-    if (req.body.signed_request) {
 
-        var base64data = req.body.signed_request.split('.', 2),
-            encodedSig = base64data[0],
-            payload = base64data[1];
-
-        try {
-            var dataStr = new Buffer(payload, 'base64').toString('ascii');
-            var data = JSON.parse(dataStr)
-            var expectedSig = crypto.createHmac('sha256', CLIENT_SECRET).update(payload).digest('hex');
-
-            if (encodedSig !== expectedSig) {
-                res.send("Bad data");
-                return;
-            }
-
-            res.send(egHTML({ token: data.oauth_token }));
-            return;
-
-        } catch(e) {
-            res.send('Could not decode request');
-            return;
-        }
+    if (!req.body.signed_request) {
+        return res.send('No signed request detected');
     }
 
-    res.send('No signed request detected');
-})
+    var [encodedSig, payload] = req.body.signed_request.split('.', 2);
+
+    try {
+        var dataStr = new Buffer(payload, 'base64').toString('ascii');
+        var data = JSON.parse(dataStr)
+        var expectedSig = crypto.createHmac('sha256', CLIENT_SECRET).update(payload).digest('hex');
+
+        if (encodedSig !== expectedSig) {
+            return res.send("Bad data");
+        }
+    } catch(e) {
+        return res.send('Could not decode request');
+    }
+
+    res.send(HTML(data));
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}.`));
+app.listen(port, () => console.log(`App listening on port ${port}.`));
+
+const HTML = (data) => `<!DOCTYPE html>
+<html>
+  <head><title>Exaple OAuth App</title></head>
+  <body>
+    <div style="text-align: center; padding-top: 10px">
+      <p>I'm an authroized app!</p>
+      <p>Token: ${data.oauth_token}</p>
+      <script>parent.postMessage("loaded", '*')</script>
+    </div>
+  </body>
+</html>`;
